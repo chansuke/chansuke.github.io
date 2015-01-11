5114d6a367b3373b439b1691976ec0564584a136
chansuke <moonset20@gmail.com>
2015-01-11 16:45:00 +0900

    update

--- site.hs
+++ site.hs
@@ -1,19 +1,24 @@
---------------------------------------------------------------------------------
 {-# LANGUAGE OverloadedStrings #-}
-import           Data.Monoid (mappend)
-import           Hakyll
 
+module Main where
 
---------------------------------------------------------------------------------
+import Control.Applicative ((<$>))
+import Data.Char (ord)
+import Data.Map (lookup)
+import Data.Monoid ((<>), mconcat, mempty)
+import Text.Pandoc (WriterOptions (..), HTMLMathMethod (MathJax))
+
+import Hakyll
+
 main :: IO ()
-main = hakyll $ do
+main = hakyllWith config $ do
     match "images/*" $ do
         route   idRoute
         compile copyFileCompiler
 
-    match "css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
+    match "css/style.scss" $ do
+        route   $ setExtension ".css"
+        compile $ getResourceString >>= sassify
 
     match (fromList ["about.rst", "contact.markdown"]) $ do
         route   $ setExtension "html"
@@ -22,43 +27,45 @@ main = hakyll $ do
             >>= relativizeUrls
 
     match "posts/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/post.html"    postCtx
-            >>= loadAndApplyTemplate "templates/default.html" postCtx
-            >>= relativizeUrls
+        route   $ setExtension "html"
+        compile $ do
+            pandocCompilerWith defaultHakyllReaderOptions pandocOptions
+            >>= saveSnapshot "content" -- ^ For RSS
+            >>= loadAndApplyTemplate "templates/post.html"
+                       (postCtx tags <> defaultContext)
+            >>= finish    (titleCtx "Blog")
 
-    create ["archive.html"] $ do
+    create ["posts.html"] $ do
+        route idRoute
+        compile $ postPage tags "All Posts" "Posts/*"
+
+    tagsRules tags $ \tag pattern -> do
+        let title = "Tag: " ++ tag
+        route idRoute
+        compile $ postPage tags title pattern
+
+    create ["index.html"] $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let archiveCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Archives"            `mappend`
-                    defaultContext
-
+            list <- postList tags "posts/*" $ fmap (take 5) . recentFirst
             makeItem ""
-                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
-                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
-                >>= relativizeUrls
+                >>= loadAndApplyTemplate "templates/index.html"
+                        (constField "posts" list <> defaultContext)
+                >>= finish (titleCtx "Home")
 
+    match "pages/*.markdown" $ do
+        route   $ composeRoutes (gsubRoute "pages/" (const "")) (setExtension ".html")
+        compile $ pandocCompiler >>= finish mempty
 
-    match "index.html" $ do
+    create ["rss.xml"] $ do
         route idRoute
         compile $ do
-            posts <- recentFirst =<< loadAll "posts/*"
-            let indexCtx =
-                    listField "posts" postCtx (return posts) `mappend`
-                    constField "title" "Home"                `mappend`
-                    defaultContext
-
-            getResourceBody
-                >>= applyAsTemplate indexCtx
-                >>= loadAndApplyTemplate "templates/default.html" indexCtx
-                >>= relativizeUrls
+            posts <- fmap (take 10) . recentFirst
+                =<< loadAllSnapshots "posts/*" "content"
+            renderRSS feedConfiguration (bodyField "description" <>
+                defaultContext) posts
 
     match "templates/*" $ compile templateCompiler
-
 
 --------------------------------------------------------------------------------
 postCtx :: Context String
